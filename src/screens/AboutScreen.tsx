7e33b8c8a3a8328857a4a13373790464326fa6b7
Jennifer Henry <jen.henry@georgebrown.ca>
2025-11-28 21:41:54 -0500

    Create empty AboutScreen component

---
+++ src/screens/AboutScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+
+const AboutScreen: React.FC = () => {
+  return (null);
+  //TODO: Implement AboutScreen content - By Faraz Safdar
+};
+
+
+
+export default AboutScreen;
